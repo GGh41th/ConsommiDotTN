ef21e3c7ff2b894bc509a7c450c7d8e665313ae2
Omar Mejdi <124160661+0marMejdi@users.noreply.github.com>
2024-04-29 16:08:06 +0100

    Added swagger
    ● Added Swagger Documentation for APIs
    ● Removed Temporarly the auth component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { log } from 'console';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('ConsommiDotTN APIs')
+    .setDescription('Endpoints for e-commerce site with some ML')
+    .setVersion('1.0')
+    .build();
+
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(PORT);
+  log("App is runnning at: http://localhost:"+PORT)
 }
+
 bootstrap();
